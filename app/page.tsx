b067e61da46644e53dcf3764d04ead36756e0047
TrueNorthHaven <info@truenorthhavenliving.com>
2026-03-20 10:09:21 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,6 @@ import Hero from "./components/Hero";
 import Community from "./components/Community";
 import Credibility from "./components/Credibility";
 import About from "./components/About";
-import OurApproach from "./components/OurApproach";
-import HousingEnvironment from "./components/HousingEnvironment";
-import Funding from "./components/Funding";
-import WhoWeServe from "./components/WhoWeServe";
-import PlacementGuidelines from "./components/PlacementGuidelines";
-import Expectations from "./components/Expectations";
-import ReferralPartners from "./components/ReferralPartners";
-import PropertyOwners from "./components/PropertyOwners";
-import PlacementProcess from "./components/PlacementProcess";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 export default function Home() {
@@ -31,16 +21,6 @@ export default function Home() {
       <Community />
       <Credibility />
       <About />
-      <OurApproach />
-      <HousingEnvironment />
-      <Funding />
-      <WhoWeServe />
-      <PlacementGuidelines />
-      <Expectations />
-      <ReferralPartners />
-      <PropertyOwners />
-      <PlacementProcess />
-      <Contact />
       <Footer />
     </main>
   );
